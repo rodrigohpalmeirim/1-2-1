3bfbdc259a83b800b240f8d9cdf6857f5b2d5e43
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2020-06-17 00:37:51 +0100

    Updates

---
+++ precache-manifest.42b543aa12c06253166a277b20747e14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed79464b7f108ae7f0a01b8ad7a4f3d9",
+    "url": "/1-2-1/index.html"
+  },
+  {
+    "revision": "90ce108bdb862832e9b6",
+    "url": "/1-2-1/static/css/main.cd5c2e92.chunk.css"
+  },
+  {
+    "revision": "7f0c9f7e9e7fcca36daa",
+    "url": "/1-2-1/static/js/2.4a0a142c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1-2-1/static/js/2.4a0a142c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90ce108bdb862832e9b6",
+    "url": "/1-2-1/static/js/main.0c81fc50.chunk.js"
+  },
+  {
+    "revision": "e1e15f690e5461b2df55",
+    "url": "/1-2-1/static/js/runtime-main.cc2f20c9.js"
+  }
+]);

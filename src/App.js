ea4a0e67c35ba97e4caaa5277cecb2dd70923652
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2020-07-17 14:36:58 +0100

    minor fix

--- src/App.js
+++ src/App.js
@@ -122,6 +122,7 @@ export default class App extends Component {
     }
     showTray = showTray.bind(this);
     document.addEventListener("mousemove", showTray);
+    document.addEventListener("click", showTray);
     document.addEventListener("touchstart", showTray);
   }
 
